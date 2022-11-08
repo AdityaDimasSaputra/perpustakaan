72a31e945fd3430cc238b01fc7f0042258de16ef
unknown <adityadmss@gmail.com>
2022-11-08 18:54:14 +0700

    first commit

---
+++ app/Controllers/Website.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Website extends BaseController
+{
+    public function index()
+    {
+        return view('website/index');
+    }
+}
---
+++ app/Views/website/layout/navbar.php
@@ -0,0 +1,59 @@
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
+        <a href="index.html" class="navbar-brand d-flex align-items-center">
+            <h6 class="m-0 text-primary"><img class="img-fluid me-2" src="../assets/img/icon-1.png" alt=""
+                    style="width: 45px;">BADAN PERPUSTAKAAN</h6>
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-4 py-lg-0">
+                <a href="index.html" class="nav-item nav-link active">HOME</a>
+                <!-- <a href="service.html" class="nav-item nav-link">Profil</a> -->
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">PROFIL</a>
+                    <div class="dropdown-menu shadow-sm m-0">
+                        <a href="feature.html" class="dropdown-item">Fasilitas</a>
+                        <a href="token.html" class="dropdown-item">Struktur Organisasi</a>
+                        <a href="faq.html" class="dropdown-item">Visi Misi</a>
+                        <a href="404.html" class="dropdown-item">Badan Perpustakaan UNTAG Surabaya</a>
+                    </div>
+                </div>
+                <!-- <a href="roadmap.html" class="nav-item nav-link">Informasi</a> -->
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">INFORMASI</a>
+                    <div class="dropdown-menu shadow-sm m-0">
+                        <a href="feature.html" class="dropdown-item">Pengumuman</a>
+                        <a href="token.html" class="dropdown-item">Berita</a>
+                        <a href="faq.html" class="dropdown-item">Keanggotaan</a>
+                        <a href="404.html" class="dropdown-item">Jam Operasional</a>
+                        <a href="404.html" class="dropdown-item">Layanan</a>
+                        <a href="404.html" class="dropdown-item">Kerjasama</a>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">PROSEDUR</a>
+                    <div class="dropdown-menu shadow-sm m-0">
+                        <a href="feature.html" class="dropdown-item">Layanan Fotokopi Skripsi</a>
+                        <a href="token.html" class="dropdown-item">Alur Kerja Proses Buku</a>
+                        <a href="faq.html" class="dropdown-item">Prosedur Pemesanan Buku Yang Akan Dipinjam</a>
+                        <a href="404.html" class="dropdown-item">Prosedur Pengembalian Buku</a>
+                        <a href="404.html" class="dropdown-item">Prosedur Peminjaman Buku</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">AKSES INFORMASI</a>
+                <a href="contact.html" class="nav-item nav-link">UNDUHAN</a>
+                <a href="contact.html" class="nav-item nav-link">KONTAK</a>
+            </div>
+            <div class="h-100 d-lg-inline-flex align-items-center d-none">
+                <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i
+                        class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i
+                        class="fab fa-twitter"></i></a>
+                <a class="btn btn-square rounded-circle bg-light text-primary me-0" href=""><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
