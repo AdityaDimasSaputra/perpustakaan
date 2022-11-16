5fea087292283fd42ea7c803c548215513370ede
unknown <adityadmss@gmail.com>
2022-11-16 14:39:10 +0700

    first commit

--- app/Views/website/layout/navbar.php
+++ app/Views/website/layout/navbar.php
@@ -31,7 +31,7 @@
 </div> -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
     <a href="index.php" class="navbar-brand d-flex align-items-center">
-        <h6 class="m-0 text-danger"><img class="img-fluid me-2" src="../assets/img/icon-1.png" alt="" style="width: 45px;">BADAN PERPUSTAKAAN UNTAG SURABAYA</h6>
+        <h2 class="m-0 text-danger">BADAN PERPUSTAKAAN</h2>
     </a>
     <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
